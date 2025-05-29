d032d2816f1c113d150238b4391ae2d622d61e10
trandat9029 <tranledatvp@gmail.com>
2025-05-29 15:33:19 +0700

    learn and config static file

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ const path = require('path');
 require('dotenv').config();
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // port => hardcode  .uat .prod
 const hostname  = process.env.HOST_NAME || 'localhost';
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
 
+//config template engine
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
+// khai bao route
 app.get('/', (req, res) => {
     res.send('hello world');
 })

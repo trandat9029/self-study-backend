560df2409375eca677843a37fcc63353dcaafce5
trandat9029 <tranledatvp@gmail.com>
2025-05-31 15:34:43 +0700

    create form add users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,19 +1,21 @@
 const connection = require('../config/database');
 
-const getHomepage = (req, res) =>{
-    //process data
-    //call model 
-    let user = [];
-    connection.query(
-    'SELECT * from Users u',
-    function(err, results, fields){
-        user = results;
-        console.log('results: ', results);
-        console.log('user: ', user);
-        res.send(JSON.stringify(user));
+// const getHomepage = (req, res) =>{
+//     //process data
+//     //call model 
+//     let user = [];
+//     connection.query(
+//     'SELECT * from Users u',
+//     function(err, results, fields){
+//         user = results;
+//         console.log('results: ', results);
+//         console.log('user: ', user);
+//         res.send(JSON.stringify(user));
         
-    }
-)
+//     }
+// )
+const getHomepage = (req, res) =>{
+   return res.render('home.ejs')
 
 }
 
